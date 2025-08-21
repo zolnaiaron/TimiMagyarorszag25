b85279020685299f73656bccc6c58ec10f356bdf
zolnaiaron <79170622+zolnaiaron@users.noreply.github.com>
2025-08-21 13:40:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const images = ["images/hatter1.jpg", "images/hatter2.jpg", "images/hatter3.jpg"];
+let currentImage = 0;
+
+const background = document.querySelector(".background");
+let xPos = 0;
+
+function animateBackground() {
+  xPos -= 1; // mozgás jobbról balra
+
+  // beállítjuk a background pozíciót
+  background.style.backgroundPosition = `${xPos}px 0`;
+
+  // ha elértük a teljes szélességet (kb. a képernyő szélessége)
+  if (Math.abs(xPos) >= window.innerWidth) {
+    currentImage = (currentImage + 1) % images.length;
+    background.style.backgroundImage = `url(${images[currentImage]})`;
+    xPos = 0;
+  }
+
+  requestAnimationFrame(animateBackground);
+}
+
+// Indítás
+background.style.backgroundImage = `url(${images[currentImage]})`;
+background.style.backgroundRepeat = "no-repeat";
+background.style.backgroundSize = "cover";
+animateBackground();
